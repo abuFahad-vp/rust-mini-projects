402d271f46642bfb0922e0dd5d9d4b479113f3fe
abuFahad-vp <fahadabu30c@gmail.com>
2024-08-19 07:52:26 +0530

    chat application completed

--- chat/client/src/main.rs
+++ chat/client/src/main.rs
@@ -1,3 +1,45 @@
+use std::{io::{self, ErrorKind, Read, Write}, net::TcpStream, sync::mpsc::{self, TryRecvError}, thread};
+
+const MSG_SIZE: usize = 1024;
+
 fn main() {
-    println!("Hello, world!");
+    let mut client = TcpStream::connect("127.0.0.1:8080").expect("Stream failed to connect");
+    client.set_nonblocking(true).expect("failed to initiate non-blocking");
+    let (tx, rx) = mpsc::channel::<String>();
+
+    thread::spawn(move || loop {
+        let mut buff = vec![0;MSG_SIZE];
+        match client.read_exact(&mut buff) {
+            Ok(_) => {
+                let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                let msg = String::from_utf8(msg).expect("failed to convert to utf-8");
+                println!("{}", msg);
+                println!("Write ur message: ");
+            }
+            Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+            Err(_) => {
+                println!("Connection with the server is ended.");
+                break;
+            }
+        }
+
+        match rx.try_recv() {
+            Ok(msg) => {
+                let mut buff = msg.clone().into_bytes();
+                buff.resize(MSG_SIZE, 0);
+                client.write_all(&buff).expect("writing to the socket failed");
+            }
+            Err(TryRecvError::Empty) => (),
+            Err(TryRecvError::Disconnected) => break
+        }
+    });
+
+    loop {
+        println!("Write ur Message: ");
+        let mut buff = String::new();
+        io::stdin().read_line(&mut buff).expect("reading from stdin failed");
+        let msg = buff.trim().to_string();
+        if msg == ":quit" || tx.send(msg).is_err() {break}
+    }
+    println!("Bye Bye!");
 }
--- chat/server/src/main.rs
+++ chat/server/src/main.rs
@@ -1,3 +1,49 @@
+use std::{io::{ErrorKind, Read, Write}, net::TcpListener, sync::mpsc, thread};
+
+const MSG_SIZE: usize = 1024;
+
 fn main() {
-    println!("Hello, world!");
+    let server = TcpListener::bind("127.0.0.1:8080").expect("Listener failed to bind");
+    server.set_nonblocking(true).expect("Failed to initialize non-blocking");
+
+    let mut clients = vec![];
+    let (tx, rx) = mpsc::channel::<String>();
+
+    println!("Server started....");
+
+    loop {
+        if let Ok((mut socket, addr)) = server.accept() {
+            println!("Client connected: {:?}", addr);
+
+            let tx = tx.clone();
+            clients.push(socket.try_clone().expect("failed to clone the client"));
+
+            thread::spawn(move || loop {
+                let mut buff = vec![0; MSG_SIZE];
+                match socket.read_exact(&mut buff) {
+                    Ok(_) => {
+                        let mut msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                        let mut formatted_msg = format!("{} said: ", addr).into_bytes();
+                        formatted_msg.append(&mut msg);
+                        let msg = String::from_utf8(formatted_msg).expect("Invaild utf8 message");
+                        println!("{}", msg);
+                        tx.send(msg).expect("Failed to send msg to rx");
+                    }
+                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+                    Err(_) => {
+                        println!("Closing the connection with {}",addr);
+                        break;
+                    }
+                }
+            });
+        }
+
+        if let Ok(msg) = rx.try_recv() {
+            clients = clients.into_iter().filter_map(|mut client| {
+                let mut buff = msg.clone().into_bytes();
+                buff.resize(MSG_SIZE, 0);
+                client.write_all(&buff).map(|_| client).ok()
+            }).collect::<Vec<_>>();
+        }
+    }
 }
